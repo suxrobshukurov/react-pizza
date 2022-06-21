a9b5987da9e42d29570928c98687234b8a4f4572
suxrobshukurov <sdead96@gmail.com>
2022-06-21 16:07:36 +0300

    create static components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Categories = () => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index)
+  };
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  return (
+    <div className="categories">
+      <ul>
+        {
+          categories.map((value, i) => (
+            <li onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''} key={i}>{value}</li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
